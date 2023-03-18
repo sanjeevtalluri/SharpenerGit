1d441202631bf4e13526e07c1d96d6eceeacff2b
sanjeevtalluri <sanjeevtalluri@gmail.com>
2023-03-18 10:20:02 +0530

    QuerySelector && QuerySelectorALL

--- index.js
+++ index.js
@@ -30,9 +30,24 @@ var listItems = document.getElementsByClassName('list-group-item');
 for(let i=0;i<listItems.length;i++){
     listItems[i].style.fontWeight = 'bold';
 }
-listItems[2].style.backgroundColor = 'green';
+//listItems[2].style.backgroundColor = 'green';
 
 var listItemsByTagName = document.getElementsByTagName('li');
 listItemsByTagName[4].classList.add('list-group-item');
 listItemsByTagName[4].style.fontWeight = 'bold';
+
+var secondListItem = document.querySelector('.list-group-item:nth-child(2)');
+secondListItem.style.backgroundColor = 'green';
+
+var thirdListItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdListItem.style.visibility = 'hidden';
+
+var listItemsQuerySelector = document.querySelectorAll('.list-group-item');
+listItemsQuerySelector[1].style.color = 'green';
+
+var listItemsOddQuerySelector = document.querySelectorAll('.list-group-item:nth-child(odd)');
+for(let i =0;i<listItemsOddQuerySelector.length;i++){
+    listItemsOddQuerySelector[i].style.backgroundColor = 'green';
+}
+
 
